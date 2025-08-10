a91582815aebc57b27fd795bbae473d3bd45dab0
danielmc-2409 <martins.cordeiro@academico.ifpb.edu.br>
2025-08-09 23:14:59 -0300

    perfil

--- app/perfpaci.tsx
+++ app/perfpaci.tsx
@@ -1,24 +1,60 @@
-import { Text, View, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
-import { Agenda, Grupo, Boneco, Lapis, Plus} from '@/assets/components/HeroIcon'; 
+import { Text, View, StyleSheet, ScrollView, TouchableOpacity, Image } from "react-native";
+import { Agenda, Grupo, Boneco, Lapis, Plus, Seta} from '@/assets/components/HeroIcon'; 
 
 
 export default function Index() {
   return (
     <View style={styles.container}>
-        <View style={styles.foto}></View>
-        <View style={styles.nome}></View>
-        <View style = {styles.infos}></View>
-        <View style = {styles.tratamento}></View>
-        
-    </View>
+        <View style={styles.foto}> 
+          <Image source={require("@/assets/images/perfil.png")} style={styles.ftdeperfil}></Image>
+        </View>
+        <View style={styles.caixanome}>
+          <Text style={styles.nome}>Lucas Henrique Duarte Silva</Text>
+        </View>
+
+        <View style = {styles.infos}>
+          <Text style={styles.nome}>Informações Detalhadas</Text>
+     
+        <TouchableOpacity onPress={() => (console.log('chegou aq'))}>  <Seta size={30} color="black" style={styles.seta}/></TouchableOpacity>
+          </View>
+      
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D9D9D9'
+    backgroundColor: '#D9D9D9',
+    alignItems:'center',
+    flexDirection:'column',
+    padding: 40
   },
 
+  ftdeperfil: {
+    width: 150,         
+    height: 150,
+    borderRadius: 100,
+    marginBottom: 20
+  },
+  nome:{
+    fontSize: 20,
+  fontWeight: 'bold',
+   textAlign: 'center',
+   marginHorizontal: 10
+},
+
+  infos:{
+    backgroundColor: "white",
+    width: '90%',
+    height: 40,
+    borderRadius: 15,
+    padding: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30
+
+  },
+ 
   
 });
--- assets/components/HeroIcon.tsx
+++ assets/components/HeroIcon.tsx
@@ -93,13 +93,17 @@ const Lixo: React.FC<HeroIconProps> = ({ size = 24, color = 'black' }) => (
 
 
 
+const Seta: React.FC<HeroIconProps> = ({ size = 24, color = 'black' }) => (
 
 
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6" width={30}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+</svg>
 
-
+);
 
 
 
 
 // Exportando ambos os componentes
-export { Agenda, Grupo, Boneco, Lapis, Plus, Lixo};
+export { Agenda, Grupo, Boneco, Lapis, Plus, Lixo, Seta};
